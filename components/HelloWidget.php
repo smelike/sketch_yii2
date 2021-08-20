311ca09ebac6e6e4e2aa9dbd5679928a7645fa87
unknown <xxj8711@gmail.com>
2021-08-20 18:40:48 +0800

    components-widget

---
+++ components/HelloWidget.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\components;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class HelloWidget extends Widget
+{
+
+  public $message;
+
+  public function init()
+  {
+    parent::init();
+    if ($this->message === null) {
+        $this->message = "Hello Widget";
+    }
+  }
+
+  public function run()
+  {
+    return Html::encode($this->message);
+  }
+}
